bae1867f61298994aac51f8553e0463e09bc3dfd
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-31 13:48:27 -0500

    spaces ok

---
+++ src/components/3D/Home3DComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-full pt-14 pl-56">
+      <div class="bg-gray-100 w-full h-full">
+
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="w-screen h-screen">
-    <div class="z-40">
+    <div class="absolute z-40">
     <HeaderGlobalComponentVue/>
     </div>
     <div class="z-30 absolute top-0 h-full">
       <LateralMenuComponentVue/>
     </div>
-    <div class="absolute top-0 h-full w-full flex items-end">      
+    <div class="z-30 absolute top-0 h-full w-full flex items-end">      
       <BottomGlobalComponentVue/>
-      </div>    
+    </div> 
   </div>
+  <div class="z-10 absolute top-0 h-screen w-screen">
+    <div class="absolute w-full h-full">
+      <Home3DComponentVue/>
+    </div>
+    </div>    
 </template>
 
 <script>
+import Home3DComponentVue from '../components/3D/Home3DComponent.vue'
 import BottomGlobalComponentVue from '../components/bottom/BottomGlobalComponent.vue'
 import HeaderGlobalComponentVue from '../components/header/HeaderGlobalComponent.vue'
 import LateralMenuComponentVue from '../components/menus/LateralMenuComponent.vue'
@@ -20,7 +26,8 @@ export default {
   components: {
     HeaderGlobalComponentVue,
     LateralMenuComponentVue,
-    BottomGlobalComponentVue
+    Home3DComponentVue,
+    BottomGlobalComponentVue,
   },
 }
 </script>
