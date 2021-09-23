9f487b054213ff990f3536078be8ce2c9085dd99
AngelIsaiRB <qwerasdffdeswa@live.com>
2021-09-23 11:07:29 -0500

    version for production v1.2.0 (hidden developer clg)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
 <div class="fixed top-0 right-0" style="z-index:60">
    <!-- Active Breakpoint Indicator -->
-<div class="fixed top-0 right-0  mt-5 mx-1 text-xs font-mono text-white h-6 w-6 rounded-full flex items-center justify-center bg-gray-700 sm:bg-pink-500 md:bg-purple-500 lg:bg-green-500 xl:bg-blue-500">
+<!-- <div class="fixed top-0 right-0  mt-5 mx-1 text-xs font-mono text-white h-6 w-6 rounded-full flex items-center justify-center bg-gray-700 sm:bg-pink-500 md:bg-purple-500 lg:bg-green-500 xl:bg-blue-500">
   <div class="block  sm:hidden md:hidden lg:hidden xl:hidden">al</div>
   <div class="hidden sm:block  md:hidden lg:hidden xl:hidden">sm</div>
   <div class="hidden sm:hidden md:block  lg:hidden xl:hidden">md</div>
   <div class="hidden sm:hidden md:hidden lg:block  xl:hidden">lg</div>
   <div class="hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden">xl</div>
   <div class="hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block">2xl</div>
-</div>
+</div> -->
 <!-- /Active Breakpoint Indicator -->
-  <p class="bg-red-400 text-sm">
-    V 1.1.8
-  </p>
+  <!-- <p class="bg-red-400 text-sm">
+    V 1.2.0
+  </p> -->
   </div>
   <router-view/>
 </template>
-
+<script >
+export default {
+  created () {
+    console.log("V 1.2.0")
+  },
+}
+</script>
 <style>
 </style>
