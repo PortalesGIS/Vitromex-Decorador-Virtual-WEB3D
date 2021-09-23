2f7e740f0afe8f82c11d98db8fcdf37a27357430
AngelIsaiRB <qwerasdffdeswa@live.com>
2021-09-23 11:23:39 -0500

    fix: change icon and site name, fix cente name serie

--- src/components/cards/SerieCards.vue
+++ src/components/cards/SerieCards.vue
@@ -9,7 +9,7 @@
                       <img :src="serie.img" class="object-cover rounded-md" style="width:140px; height:140px" alt="">   
                      </div>                                                        
                      <div class="absolute top-0 w-full h-full flex justify-center items-center cursor-pointer">
-                       <p class="text-white font-bold text-base moserrat-bold">{{serie.name}}</p>
+                       <p class="text-white font-bold text-center text-base moserrat-bold">{{serie.name}}</p>
                      </div>
                     </div>
                   </div>
@@ -25,7 +25,7 @@
                       <img :src="serie.img" class="object-cover rounded-md" style="width:140px; height:140px" alt="">   
                      </div>                                                        
                      <div class="absolute top-0 w-full h-full flex justify-center items-center">
-                       <p class="text-white font-bold text-base">{{serie.name}}</p>
+                       <p class="text-white text-center font-bold text-base">{{serie.name}}</p>
                      </div>
                    </div>
                   </div>
@@ -40,7 +40,7 @@
           <img :src="serieSelected.render" class="object-cover w-full rounded-md h-24 lg:h-20"  alt="">   
          </div>                                                        
          <div class="absolute top-0 w-full h-full flex justify-center items-center">
-           <p class="text-white font-bold text-base moserrat-bold">{{serieSelected.name}}</p>
+           <p class="text-white font-bold text-center text-base moserrat-bold">{{serieSelected.name}}</p>
          </div>
          <div class="absolute top-0 w-full h-full flex justify-end items-start px-1 py-1 ">
            <img src="../../assets/web/Cerrar_B.svg" alt="" class="w-8 cursor-pointer">
--- src/views/Arko.vue
+++ src/views/Arko.vue
@@ -15,6 +15,12 @@ export default {
         ...mapActions(['onChangeStatePage','entryWebAnalytics'])
     },
     created () {
+    document.title = "Mi proyecto Arko"
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = '/faviconark.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
         this.onChangeStatePage(true)
         this.entryWebAnalytics()
     },
