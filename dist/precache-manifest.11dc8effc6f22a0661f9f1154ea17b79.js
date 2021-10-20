180ced93f415b19df1bad21b0fbbf1a3e14ddd55
AngelIsaiRB <qwerasdffdeswa@live.com>
2021-10-20 16:40:38 -0500

    add dist to upload inmersysapps web

---
+++ dist/precache-manifest.11dc8effc6f22a0661f9f1154ea17b79.js
@@ -0,0 +1,554 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "908bce02bbe0cff9a88a",
+    "url": "/css/app.ec57463b.css"
+  },
+  {
+    "revision": "39bbdfaecc6ccf4273204ace3a484c81",
+    "url": "/faviconark.ico"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/fonts/Gotham_Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "9249716e1e9daf627a5beec6b4a5adaf",
+    "url": "/fonts/Gotham_Light.9249716e.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/fonts/Montserrat_Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/fonts/Montserrat_Light.100b38fa.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/fonts/Montserrat_Regular.9c460951.ttf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/fonts/Montserrat_SemiBold.bb3740d3.otf"
+  },
+  {
+    "revision": "4d8dd48c299083ee70ed29a157804280",
+    "url": "/img/ARKO_Logo.4d8dd48c.svg"
+  },
+  {
+    "revision": "26756df9cd72fcf6ec3d15135a3dbe8a",
+    "url": "/img/Buscar.26756df9.svg"
+  },
+  {
+    "revision": "a152663f5406f9ab18d0431d182c078c",
+    "url": "/img/Buscar.a152663f.svg"
+  },
+  {
+    "revision": "4ddcf0fcc71f63f23e2a7e68766c4048",
+    "url": "/img/Cerrar.4ddcf0fc.svg"
+  },
+  {
+    "revision": "d3757f318599e4c012735c854a10d513",
+    "url": "/img/Cerrar_B.d3757f31.svg"
+  },
+  {
+    "revision": "ee54d0510443a1a9f9f269b8c5c4c478",
+    "url": "/img/Cerrar_Dropdown.ee54d051.svg"
+  },
+  {
+    "revision": "3726774813c5ef50af3460b5ec7369d9",
+    "url": "/img/Cerrar_overlay.37267748.svg"
+  },
+  {
+    "revision": "1a529a89368c14687fbab7b0ddd0b037",
+    "url": "/img/Dropdown.1a529a89.svg"
+  },
+  {
+    "revision": "48ce9a9202df4f6ee6eeb6dfdaee0aa8",
+    "url": "/img/Dropdown.48ce9a92.svg"
+  },
+  {
+    "revision": "9d9621c56dbe5f5df0b11d31a12757c5",
+    "url": "/img/Filtros.9d9621c5.svg"
+  },
+  {
+    "revision": "fdc47aa40e631a61dedc4a201b0f114b",
+    "url": "/img/Filtros.fdc47aa4.svg"
+  },
+  {
+    "revision": "06bd55e24080429591845f065426506a",
+    "url": "/img/Fondo.06bd55e2.png"
+  },
+  {
+    "revision": "8c02445a8e324bb85cbcc125c28b319e",
+    "url": "/img/Header.8c02445a.png"
+  },
+  {
+    "revision": "f09177bc1948360d1126ea4b04a5a374",
+    "url": "/img/Header.f09177bc.png"
+  },
+  {
+    "revision": "f02cefdcb595d7692f8c0c6fddda0bb5",
+    "url": "/img/Info.f02cefdc.svg"
+  },
+  {
+    "revision": "38af764139c8d265e597691d60b1e900",
+    "url": "/img/Info_B.38af7641.svg"
+  },
+  {
+    "revision": "7ae650844e4860d20c1a969791b61ee9",
+    "url": "/img/Logo_Vitromex.7ae65084.svg"
+  },
+  {
+    "revision": "b90468be0998d8649dda2abdb0ec5017",
+    "url": "/img/Menu.b90468be.svg"
+  },
+  {
+    "revision": "69ad9d8c840af533acf4c801d33384c0",
+    "url": "/img/Menu_B.69ad9d8c.svg"
+  },
+  {
+    "revision": "fc9cfe8fcf8c1c5d68d087ec335b3fc8",
+    "url": "/img/Piso.fc9cfe8f.svg"
+  },
+  {
+    "revision": "39c3ad2ecabdcf8317fa108f832e7f60",
+    "url": "/img/Productos.39c3ad2e.svg"
+  },
+  {
+    "revision": "10397b83a5bf08c0b2a35b7d18f795f4",
+    "url": "/img/cerrar_dropdown.10397b83.svg"
+  },
+  {
+    "revision": "6c4126739b7fa874e975a37b731015a4",
+    "url": "/img/cerrar_filtro_catalogo.6c412673.svg"
+  },
+  {
+    "revision": "7145041d1d9777620af218a6ff8197fc",
+    "url": "/img/cerrar_filtro_catalogo.7145041d.svg"
+  },
+  {
+    "revision": "a5a58bc8249a2fd42da4e8865060599e",
+    "url": "/img/cerrar_menu.a5a58bc8.svg"
+  },
+  {
+    "revision": "24cd7f92e0588bd0fa965bf5390a472d",
+    "url": "/img/fondoarko.24cd7f92.png"
+  },
+  {
+    "revision": "f959bdd3d6d996eb21ca5cc05b35418d",
+    "url": "/img/not_found.f959bdd3.png"
+  },
+  {
+    "revision": "151cebeea63e203882a509a10a89e372",
+    "url": "/img/tutorial_icon.151cebee.svg"
+  },
+  {
+    "revision": "f5d9568e74ab9a53adaa0d28a2398fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908bce02bbe0cff9a88a",
+    "url": "/js/app.432ce254.js"
+  },
+  {
+    "revision": "5f77aede3dfe57aa8376",
+    "url": "/js/chunk-vendors.ddb7910d.js"
+  },
+  {
+    "revision": "26ecd16b95e66e3d92a8269b48b56903",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "dc20a8cf5d57ef54f36b026952fab44b",
+    "url": "/models3D/enviroment/+X.jpg"
+  },
+  {
+    "revision": "f0facd849cd6d9e89d96e04d66af8ab4",
+    "url": "/models3D/enviroment/+Y.jpg"
+  },
+  {
+    "revision": "e7a7978217f677908a1fc52362ab6f01",
+    "url": "/models3D/enviroment/+Z.jpg"
+  },
+  {
+    "revision": "7c0de9c52963fa0659868554cbac56f5",
+    "url": "/models3D/enviroment/-X.jpg"
+  },
+  {
+    "revision": "cdd3c87ce69734920347020b774b8c97",
+    "url": "/models3D/enviroment/-Y.jpg"
+  },
+  {
+    "revision": "12b6195800aa99ea17597773bc3a252f",
+    "url": "/models3D/enviroment/-Z.jpg"
+  },
+  {
+    "revision": "eb75d6df7d824e83940e2c7f2ef65c70",
+    "url": "/models3D/enviroment/Enviroment_Interior.hdr"
+  },
+  {
+    "revision": "3bb054a70610731640dcf7b4c1b87f78",
+    "url": "/models3D/house-v1/license.txt"
+  },
+  {
+    "revision": "1de115ca2e10f3c39b170a46042ada32",
+    "url": "/models3D/house-v1/light_maps/UV_a.jpg"
+  },
+  {
+    "revision": "2135c25563f1ffa58a92983d2f86caa8",
+    "url": "/models3D/house-v1/light_maps/UV_b.jpg"
+  },
+  {
+    "revision": "3437701086a3486e6aa6a6550af93462",
+    "url": "/models3D/house-v1/light_maps/UV_c.jpg"
+  },
+  {
+    "revision": "6ff4d04d89b6980ac272e108ac8b8eb1",
+    "url": "/models3D/house-v1/light_maps/UV_d.jpg"
+  },
+  {
+    "revision": "daa5955b1ef54167428d47f8c5dd3f74",
+    "url": "/models3D/house-v1/light_maps/UV_e.jpg"
+  },
+  {
+    "revision": "015663825e6efa195aeadfb389ba83e9",
+    "url": "/models3D/house-v1/light_maps/UV_f.jpg"
+  },
+  {
+    "revision": "8cfc8a8cb3d0b65f5983ff506710bb0a",
+    "url": "/models3D/house-v1/light_maps/white.jpg"
+  },
+  {
+    "revision": "93780382d8edfb9ff3142c172a30cc80",
+    "url": "/models3D/house-v1/scene.bin"
+  },
+  {
+    "revision": "33631dd4678d2935c693885ee8b17c5e",
+    "url": "/models3D/house-v1/scene.gltf"
+  },
+  {
+    "revision": "2699ebb4c96ab034786a5beba1b4602f",
+    "url": "/models3D/house-v1/textures/Amb_Base_Banio_Jabon_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "856fd9e0bccac3e895e7ade07440a03c",
+    "url": "/models3D/house-v1/textures/Amb_Base_Cocina_Olla_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "dcbb8ce345bb5d8237cc2767d5b6d379",
+    "url": "/models3D/house-v1/textures/Amb_Base_Cocina_tabla_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "761bdae1fa16222dce42cdfdb29306be",
+    "url": "/models3D/house-v1/textures/Amb_Base_Cocina_tabla_UVf_metallicRoughness.png"
+  },
+  {
+    "revision": "b1526e61bb5e82e652e501c2106a6a1e",
+    "url": "/models3D/house-v1/textures/Amb_Base_Patio_AgavePina_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "5039f6c23e35dcac94a9534e8296ea52",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Banio_Lavabo_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "b081ab6af6ef103e34c9c26378a2dc5d",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Banio_Lavabo_UVf_metallicRoughness.png"
+  },
+  {
+    "revision": "cab61fdb1266b0f89da07f510d18e338",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Banio_Toallas_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "f079db6416ecbf2fe7b313478bf871a1",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Banio_WC_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "4040bbd3d82d962e176836d8a3aea09d",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Banio_WC_UVf_metallicRoughness.png"
+  },
+  {
+    "revision": "abcff0f63aebbc2ce3a2e4006fbc4611",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Carpinterias_UVc_baseColor.jpeg"
+  },
+  {
+    "revision": "553412a89ff18c6e8049baa27a269ca5",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Carpinterias_UVc_normal.png"
+  },
+  {
+    "revision": "7121d60311c9eb267f6869cb3ee51149",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Cocina_Limas_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "6f3ec3484fbff1020adf181906b39f0a",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Comedor_Centromesa_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "e1966972333d6b18ebaa6670d309b97d",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Comedor_Lampara_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "1fcf02f4ea8549d82c5cf9c5a4a0ec17",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Comedor_Mantel_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "543d856df51a09ff0af1ddff847b8b46",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_General_Libros_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "855b8f6c6a40268d2f4a77fc8d590341",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_General_PlantaChica_UVf_baseColor.png"
+  },
+  {
+    "revision": "afdbb523fb9b6c7cbb6d39b66e7fc043",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_General_PlantaGrande_UVf_baseColor.png"
+  },
+  {
+    "revision": "ea28aaeb8d0e658d733a9759475eab48",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_General_PlantaPalmera_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "7f7d8a0c41485243d606613a8b2779ff",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Patio_Agave_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "c828f3953288fa35edc9b2a31f74bcb6",
+    "url": "/models3D/house-v1/textures/MT_Amb_Base_Sala_Pinturas_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "3abb579151a278b0bc40b5f08b9f5a1b",
+    "url": "/models3D/house-v1/textures/MT_Losa_Base_Int_Cocina_UVb_baseColor.jpeg"
+  },
+  {
+    "revision": "e0b7c7165b4ef7b7f606fdfd4ba07920",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Cocina_Alacenas_UVd_baseColor.jpeg"
+  },
+  {
+    "revision": "728c89616351d35195f6a389b8697ec1",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Cocina_Alacenas_UVd_metallicRoughness.png"
+  },
+  {
+    "revision": "f7a050f2ae989b267fbea87007908c07",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Cocina_Lamparas_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "c59ba1c960de44596025af86017c3e98",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Cocina_Microondas_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "7cb0e4afabfb6b7245673a30a5ca0e11",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Cocina_Refrigerador_UVd_baseColor.jpeg"
+  },
+  {
+    "revision": "4f39dd30c9be2b6a7b4441506cb4e0f6",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Macetas_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "768968e807daeca269dc8161d4832025",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Macetas_UVe_normal.png"
+  },
+  {
+    "revision": "76d0c686f3a1e4adb292b4da40e1be65",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Sala_Lampara_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "fa34d711c18ade597f4901237366696b",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Sala_Sillon_UVe_baseColor.png"
+  },
+  {
+    "revision": "7682218ac7713138a78f24d6b06ee50c",
+    "url": "/models3D/house-v1/textures/MT_Mob_Base_Sala_Sillon_UVe_metallicRoughness.png"
+  },
+  {
+    "revision": "d4e53e72ac9132872a304a8f2273b1b2",
+    "url": "/models3D/house-v1/textures/MT_Mod_Base_Skybox_baseColor.jpeg"
+  },
+  {
+    "revision": "d4e53e72ac9132872a304a8f2273b1b2",
+    "url": "/models3D/house-v1/textures/MT_Mod_Base_Skybox_emissive.jpeg"
+  },
+  {
+    "revision": "5d0f5313562032a16a55af846ce7f4df",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Ext_Patio_Arriba_UVa_baseColor.jpeg"
+  },
+  {
+    "revision": "46b8beea6cf3392e3d7d3bbd7c011607",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Ext_Patio_Der_UVa_baseColor.jpeg"
+  },
+  {
+    "revision": "0f668d1aa70242803dc5e9151f1bb086",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Ext_Patio_Der_UVa_normal.png"
+  },
+  {
+    "revision": "ec57b3b2b516699ea1c614b618735e47",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Banio_Izq2_UVa_baseColor.jpeg"
+  },
+  {
+    "revision": "e2f5fd6038bd8656244c0ff52dd025c4",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Banio_Izq2_UVa_normal.png"
+  },
+  {
+    "revision": "1d6d38cc2647a30e41f11371ad297eb0",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Cocina_Front_UVa_baseColor.jpeg"
+  },
+  {
+    "revision": "9b55d03a796c7df2b55f206b51e2ddd6",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Cocina_Front_UVa_normal.png"
+  },
+  {
+    "revision": "fd8a8c525dff3fbf69481b65064f6805",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Sala_Der_UVa_normal.png"
+  },
+  {
+    "revision": "e57985bf3dd51995e92339dcca4eaec5",
+    "url": "/models3D/house-v1/textures/MT_Muro_C_Int_Sala_Front_UVa_baseColor.jpeg"
+  },
+  {
+    "revision": "3c065de7ae6940483c2bc2a5a61f3f67",
+    "url": "/models3D/house-v1/textures/MT_Piso_Base_Ext_Grava_UVb_baseColor.jpeg"
+  },
+  {
+    "revision": "f70dfa404b939362eb6f0a1425c5e285",
+    "url": "/models3D/house-v1/textures/MT_Piso_Base_Ext_Grava_UVb_metallicRoughness.png"
+  },
+  {
+    "revision": "c4d11778aecb23c94bb988b4d1ed044e",
+    "url": "/models3D/house-v1/textures/MT_Piso_C_Ext_Pasillo_UVb_baseColor.jpeg"
+  },
+  {
+    "revision": "690afce3e7498c8365d0a56fba358639",
+    "url": "/models3D/house-v1/textures/Material_81_baseColor.jpeg"
+  },
+  {
+    "revision": "d8bbf9c69e6cda2566cb6711e4a6efb5",
+    "url": "/models3D/house-v1/textures/Mob_Base_Cocina_Fregaderoyestufa_UVd_baseColor.jpeg"
+  },
+  {
+    "revision": "5f457ddb3a824bd323126ed39975ea41",
+    "url": "/models3D/house-v1/textures/Mob_Base_Comedor_Mesa_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "596c005758eaf75196ce4230f42e5a94",
+    "url": "/models3D/house-v1/textures/Mob_Base_Comedor_Sillas_UVe_baseColor.png"
+  },
+  {
+    "revision": "859c3c73080cf8d516718a86080595bc",
+    "url": "/models3D/house-v1/textures/Mob_Base_Sala_2Sillones_UVe_baseColor.jpeg"
+  },
+  {
+    "revision": "e06ebb313ba786be1a232b28a1c23c4b",
+    "url": "/models3D/house-v1/textures/Mob_Base_Sala_2Sillones_UVe_normal.png"
+  },
+  {
+    "revision": "64722e0358745905a10697b8522e6bae",
+    "url": "/models3D/house-v1/texturesseparates/MT_Amb_Base_General_PlantaChica_UVf_baseColor.png"
+  },
+  {
+    "revision": "ea28aaeb8d0e658d733a9759475eab48",
+    "url": "/models3D/house-v1/texturesseparates/MT_Amb_Base_General_PlantaPalmera_UVf_baseColor.jpeg"
+  },
+  {
+    "revision": "8a954a565192f28a57dfafbbde13b9e4",
+    "url": "/models3D/house-v1/texturesseparates/ad.png"
+  },
+  {
+    "revision": "7e18570ec942ccbcdf104657982b4c2f",
+    "url": "/models3D/house-v1/texturesseparates/signal_hill_dawn_2k.jpg"
+  },
+  {
+    "revision": "4d7a33ab30b4d8bb30ac65a8467ef312",
+    "url": "/models3D/hovers/license.txt"
+  },
+  {
+    "revision": "b4deafd04f88949b69ee65be9f3e543b",
+    "url": "/models3D/hovers/scene.bin"
+  },
+  {
+    "revision": "fc7459d4292abcf8a8729059d3a5b6bc",
+    "url": "/models3D/hovers/scene.gltf"
+  },
+  {
+    "revision": "df8dcf6c4f9dc3416b0d79ab15a6d7f4",
+    "url": "/models3D/hovers/textures/Dots.png"
+  },
+  {
+    "revision": "fb21ba0b28513d74d8d1f6c6755a75bb",
+    "url": "/models3D/hovers/textures/MT_Hover_Piso_C_Int_Banio2_UVb_diffuse.jpeg"
+  },
+  {
+    "revision": "a2f9a377224450671237440ce3e18470",
+    "url": "/models3D/img/cursor.png"
+  },
+  {
+    "revision": "3bb054a70610731640dcf7b4c1b87f78",
+    "url": "/models3D/lights/license.txt"
+  },
+  {
+    "revision": "1e14dbcb68bc687b80c650ad86ef19f1",
+    "url": "/models3D/lights/scene.bin"
+  },
+  {
+    "revision": "818d249d2fd9e4d8c3d518c9a24a54bc",
+    "url": "/models3D/lights/scene.gltf"
+  },
+  {
+    "revision": "5d78269e608e842d6b39d3c7abc70fdf",
+    "url": "/models3D/lights/textures/Material_3232_baseColor.png"
+  },
+  {
+    "revision": "f4433e62228cf3255690d3777f9d22a1",
+    "url": "/models3D/lights/textures/Material_3232_emissive.png"
+  },
+  {
+    "revision": "259c386468845e7ddfcf34e52b0bd220",
+    "url": "/models3D/lights/textures/Material_3232_normal.png"
+  },
+  {
+    "revision": "ecddc0f8daa8800f05875fab68cf155a",
+    "url": "/models3D/test/Lightmap-0_comp_light_m.png"
+  },
+  {
+    "revision": "c5e4df4a9fc862994ca9b6efa2d064a5",
+    "url": "/models3D/test/Lightmap-0_comp_light_rgb.png"
+  },
+  {
+    "revision": "24b9d1612dbfeae9d806095de7f0f55a",
+    "url": "/models3D/test/license.txt"
+  },
+  {
+    "revision": "856a945c86f3a46cf03679a8a8c3fa4b",
+    "url": "/models3D/test/scene.bin"
+  },
+  {
+    "revision": "37ff9da35d9018f5cde63f9cfd0f2f01",
+    "url": "/models3D/test/scene.gltf"
+  },
+  {
+    "revision": "e82ca3679ff4ef8c08517471ae50431f",
+    "url": "/models3D/test/textures/mt_piso_prueba_baseColor.jpeg"
+  },
+  {
+    "revision": "4a615464301fcee39c9ff09e72f1d952",
+    "url": "/models3D/windowFachada/license.txt"
+  },
+  {
+    "revision": "cfbca36c3a85546cd8a23ed586185856",
+    "url": "/models3D/windowFachada/scene.bin"
+  },
+  {
+    "revision": "481037b5a64db3142ff2fb79f9cb5a48",
+    "url": "/models3D/windowFachada/scene.gltf"
+  },
+  {
+    "revision": "761bdae1fa16222dce42cdfdb29306be",
+    "url": "/models3D/windowFachada/textures/MT_Amb_Base_vidrioopaco2_UVc_metallicRoughness.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
